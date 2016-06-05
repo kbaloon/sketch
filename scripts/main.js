c842179a60ce54039f94665ca359a0f06d15d342
Kikito <kikitobaloon@yahoo.com>
2016-06-06 00:11:44 +0200

    el grid i el hover funcionen
    Es la base del sketch

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-	for (var j = 0; j < 50; j++) {
-		$(".container").append($("<div class=\"row"+j+"\"></div>"));
-		var $addedRow = $(".row" + j);
-		for (var i = 0; i < 50; i++) {
+	var matrix = 100;
+	for (var j = 0; j < matrix; j++) {
+		$(".container").append($("<div class =\"row\" id=\"row"+j+"\"></div>"));
+		var $addedRow = $("#row" + j);
+		for (var i = 0; i < matrix; i++) {
 			$addedRow.append($("<div class=\"cell\"></div>"));
 		};
-
-
 	};
+	var $c = $('.cell');
+	$c.hover(function() {
+		$(this).css("background-color","yellow");
+	});
 })
