05470c69f3e4e67e79dd6a58995869e99b1739c7
kbaloon <kbaloon@users.noreply.github.com>
2016-06-07 18:16:34 +0200

    WIth prompt and Clear buttons
    Uploaded from Lenovo twist

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('#drawB').click(function() {
+		var matrix = prompt('Set the size of the Matrix', 50);
+		var $cont = $(".container");
+		for (var j = 0; j < matrix; j++) {
+			$cont.append($("<div class =\"row\" id=\"row"+j+"\"></div>"));
+			var $addedRow = $("#row" + j);
+			for (var i = 0; i < matrix; i++) {
+				$addedRow.append($("<div class=\"cell\"></div>"));
+			};
+		};
+		var $c = $('.cell');
+		$c.hover(function() {
+			$(this).css("background-color","yellow");
+		});
+	});
+	//clear Button - removes the container dive that we've used and adds a clean one 
+	$('#clearB').click(function() {
+		$(".container").remove();
+		$("body").append("<div class=\"container\" style=\"display: table;\"><div>");
+	});
+})
